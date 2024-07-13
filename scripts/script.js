a1795d50b97c79c3a726949af04925d484670839
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-07-13 10:23:50 +0300

    Added carousel

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,44 @@
+// Copy button
 const copyBtn = document.querySelector('.copy-btn');
 const code = document.querySelector('.code');
 
 copyBtn.addEventListener('click', () => {
   navigator.clipboard.writeText(code.textContent);
 })
+
+// String carousel
+const createCarousel = (selector) => {
+  const splide = new Splide(selector, {
+    type   : 'loop',
+    drag   : 'free',
+    focus  : 'center',
+    perPage: 16,
+    arrows: false,
+    pagination: false,
+    drag: false,
+    autoScroll: {
+      speed: 1,
+    },
+    breakpoints: {
+      1800: {
+        perPage: 12,
+      },
+      1600: {
+        perPage: 10,
+      },
+      1200: {
+        perPage: 8,
+      },
+      900: {
+        perPage: 6,
+      },
+      600: {
+        perPage: 5,
+      },
+    }
+  });
+  splide.mount(window.splide.Extensions);
+}
+
+createCarousel('.promo-string-carousel');
+createCarousel('.promo-string-carousel-two');
